46419ef2d515c876075b1545814de1aea4cda753
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-10-16 13:15:30 +0530

    Update reporter.go

--- modules/agent/cron/reporter.go
+++ modules/agent/cron/reporter.go
@@ -16,8 +16,8 @@ package cron
 
 import (
 	"fmt"
-	"github.com/open-falcon/falcon-plus/common/model"
-	"github.com/open-falcon/falcon-plus/modules/agent/g"
+	"github.com/mohneesh9797-puresoftware/falcon-plus/common/model"
+	"github.com/mohneesh9797-puresoftware/falcon-plus/modules/agent/g"
 	"log"
 	"time"
 )
