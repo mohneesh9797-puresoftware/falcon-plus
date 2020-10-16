d94c1abe6dc1606dc8e60c577b90ce2778a52249
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-10-16 12:43:50 +0530

    Update docker_test.sh

--- docker_test.sh
+++ docker_test.sh
@@ -9,12 +9,12 @@ export REDIS_PORT=16379
 export API_PORT=18080
 export API_HOST=127.0.0.1
 git clone https://github.com/mohneesh9797-puresoftware/mysql
-cd mysql
-cd 5.7
-docker build -t mysql .
-cd ../../
+#cd mysql
+#cd 5.7
+#docker build -t mysql .
+#cd ../../
 #docker rm -f falcon-mysql falcon-redis falcon-plus &> /dev/null
-docker run --name falcon-mysql -e MYSQL_ROOT_PASSWORD=$DB_PASSWORD -p $DB_PORT:3306 -d mysql
+docker run --name falcon-mysql -e MYSQL_ROOT_PASSWORD=$DB_PASSWORD -p $DB_PORT:3306 -d mysql/mysql-server:8.0.21-1.1.17
 docker run --name falcon-redis -p $REDIS_PORT:6379 -d redis:4-alpine3.8
 
 echo "waiting mysql start..."
