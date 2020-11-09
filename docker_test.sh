c3242cfcbc0fb280c6086e4874e094e1606cf141
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-11-09 09:14:15 +0530

    Update docker_test.sh

--- docker_test.sh
+++ docker_test.sh
@@ -11,9 +11,9 @@ export API_HOST=127.0.0.1
 
 docker rm -f falcon-mysql falcon-redis falcon-plus &> /dev/null
 if [[ `uname -m` == "aarch64" ]]; then
-	docker run --name falcon-mysql -e MYSQL_ROOT_PASSWORD=$DB_PASSWORD -p $DB_PORT:3306 -d mysql/mysql-server
-else
 	docker run --name falcon-mysql -e MYSQL_ROOT_PASSWORD=$DB_PASSWORD -p $DB_PORT:3306 -d mariadb:10.1
+else
+	docker run --name falcon-mysql -e MYSQL_ROOT_PASSWORD=$DB_PASSWORD -p $DB_PORT:3306 -d mysql:5.7
 fi
 docker run --name falcon-redis -p $REDIS_PORT:6379 -d redis:4-alpine3.8
 
